8dc6da8a5f921dc1bf6a49f5f2b19d81c311d28a
Elmer <elmer.izarra@gmail.com>
2023-06-07 21:52:41 -0500

    conexion server

--- myclientql.cpp
+++ myclientql.cpp
@@ -6,10 +6,46 @@ MyClientQl::MyClientQl(QWidget *parent)
     , ui(new Ui::MyClientQl)
 {
     ui->setupUi(this);
+    clientSocket = new QTcpSocket(this);
+    in.setDevice(clientSocket);
+    connect(ui->btnClose, &QAbstractButton::clicked, this, &QWidget::close);
+    connect(ui->btnConnect, &QAbstractButton::clicked, this, &MyClientQl::requestMessage);
+    // escucha la respuesta del servidor a la request que se realizo.
+    connect(clientSocket, &QIODevice::readyRead, this, &MyClientQl::readMessage);
+    connect(clientSocket, &QIODevice::destroyed, this, &MyClientQl::closedCNX);
 }
 
 MyClientQl::~MyClientQl()
 {
     delete ui;
+}
+
+void MyClientQl::requestMessage()
+{
+    qDebug() << "request message";
+    QString ipAddress = ui->textIp->text();
+    QString port = ui->textPort->text();
+    clientSocket->connectToHost(ipAddress, port.toInt());
+    qDebug() << "ip: " + ipAddress;
+    qDebug() << "port: " + port;
+}
+
+void MyClientQl::readMessage()
+{
+    qDebug() << "readMessage ...";
+    in.startTransaction();
+    QString message;
+    // divice a la izquierda >> string
+    in >> message;
+    if (!in.commitTransaction()) {
+        qDebug() << "error in data stream";
+        return;
+    }
+    ui->lblMessage->setText(message);
+}
+
+void MyClientQl::closedCNX()
+{
+    qDebug() << "closeCNX";
 }
 
--- myclientql.h
+++ myclientql.h
@@ -2,6 +2,7 @@
 #define MYCLIENTQL_H
 
 #include <QWidget>
+#include <QTcpSocket>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MyClientQl; }
@@ -15,7 +16,15 @@ public:
     MyClientQl(QWidget *parent = nullptr);
     ~MyClientQl();
 
+private slots:
+    void requestMessage();
+    void readMessage();
+    void closedCNX();
+
 private:
     Ui::MyClientQl *ui;
+    QTcpSocket *clientSocket = nullptr;
+    QDataStream in;
+    QString receiveMessage;
 };
 #endif // MYCLIENTQL_H
